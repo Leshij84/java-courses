8be57724cd7045f267c3d11294b5f1150c8f2e26
Leshij84 <Leshij_84@tut.by>
2017-06-13 21:58:50 +0300

    update Calculate

--- Calculate.java
+++ Calculate.java
@@ -1,6 +1,26 @@
 public class Calculate {
 
 	public static void main(String[] arg) {
-		System.out.println("Calculate...");	
+		System.out.println("Calculate...");
+		int first = Integer.valueOf(arg[0]);
+		int second = Integer.valueOf(arg[2]);
+		int result = 0;
+		if (arg[1].equals("+")) {
+		result = first + second;
+		System.out.print("Sum ");
+		}
+		if (arg[1].equals("-")) {
+		result = first - second;
+		System.out.print("Difference ");
+		}
+		if (arg[1].equals("/")) {
+		result = first / second;
+		System.out.print("Division ");
+		}
+		if (arg[1].equals("++")) {
+		result = first * second;
+		System.out.print("Multiplication ");
+		}
+		System.out.print(result);	
 }
 }
